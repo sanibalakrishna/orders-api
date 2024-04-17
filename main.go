4059f571a1b82bc8dbf088f1b8eb13f6f7a6e7fc
sanibalakrishna <sanibalakrishna@gmail.com>
2024-04-18 02:42:13 +0530

    added Appliction/ structure

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/go-chi/chi/v5"
- "github.com/go-chi/chi/v5/middleware"
+	"context"
+	"fmt"
 
-  
+	"github.com/sanibalakrishna/orders-api/application"
 )
 
-func main(){
-  router := chi.NewRouter()
-  router.Use(middleware.Logger)
-  router.Get("/hello",basicHandler)
-  server := &http.Server{
-    Addr: ":3000",
-    Handler: router,
-  }
-  err:= server.ListenAndServe()
-  if err != nil{
-  fmt.Println("Failed to Load the Server",err)
-  }
+func main() {
+	app := application.New()
 
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start the app:", err)
+	}
 }
-
-func basicHandler(w http.ResponseWriter,r *http.Request){
-w.Write([]byte("Hello, world!"))}
